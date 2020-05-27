941ed5ff96aff60c7f0f5667bba9a8aa45c069c6
Jonathan K <phenixdoc@gmail.com>
2020-05-26 21:45:42 -0700

    change manifest version to master

--- bungie.manifest.js
+++ bungie.manifest.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const BUNGIE_MANIFEST_VERSION = '83341.20.04.17.1921-8';
+const BUNGIE_MANIFEST_VERSION = '000';
+// const GIT_BRANCH = `bungie-manifest-version-${BUNGIE_MANIFEST_VERSION}`;
+const GIT_BRANCH = 'master';
 
 class BungieManifest {
 
   constructor() {
-		this._manifest = JSON.parse(UrlFetchApp.fetch(`https://raw.githubusercontent.com/legion0/llbalancer/bungie-manifest-version-${BUNGIE_MANIFEST_VERSION}/bungie.manifest.json`).getContentText());
+		this._manifest = JSON.parse(UrlFetchApp.fetch(`https://raw.githubusercontent.com/legion0/llbalancer/${GIT_BRANCH}/bungie.manifest.json`).getContentText());
 	}
 	
 	getItemCategories(itemHash) {
